0bfd82f276860b2d61dfc319adc7d2dd55e328ad
Rajesh2105280070 <rajesh.pattanaik2000@gmail.com>
2024-12-06 13:05:50 +0530

    Second Commit adding some dependencies

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+// import 'react-native-gesture-handler';
+import SplashScreen from './src/screens/loginScreens/SplashScreen';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => SplashScreen);
